c2ff196f04557c76028fe5a8a9aa57a2d3d60cdf
Walt Sorensen <photodude@users.noreply.github.com>
2016-08-27 17:55:30 -0600

    try fixing hhvm

--- build/travis/unit-tests.sh
+++ build/travis/unit-tests.sh
@@ -15,6 +15,11 @@ if [[ $TRAVIS_PHP_VERSION = hhv* ]]; then echo 'xdebug.enable = 0' >> /etc/hhvm/
 
 # Make sure all dev dependencies are installed
 composer install
+if [[ $HHVMPHP7 == "yes" ]]; then echo hhvm.php7.all=1 >> /etc/hhvm/php.ini; fi
+if [[ $TRAVIS_PHP_VERSION = hhvm-3.12 ]]; then sudo wget --directory-prefix=./ https://github.com/PocketRent/hhvm-pgsql/raw/releases/3.12.0/ubuntu/trusty/pgsql.so; fi
+if [[ $TRAVIS_PHP_VERSION = hhvm ]]; then sudo wget --directory-prefix=./ https://github.com/PocketRent/hhvm-pgsql/raw/releases/3.14.0/ubuntu/trusty/pgsql.so; fi
+if [[ $TRAVIS_PHP_VERSION = hhvm-3.12 ]]; then echo hhvm.dynamic_extensions[pgsql] = pgsql.so >> /etc/hhvm/php.ini; fi
+if [[ $TRAVIS_PHP_VERSION = hhvm ]]; then echo hhvm.dynamic_extensions[pgsql] = pgsql.so >> /etc/hhvm/php.ini; fi
 
 # Setup databases for testing
 mysql -u root -e 'create database joomla_ut;'
@@ -28,9 +33,9 @@ psql -d joomla_ut -a -f "$BASE/tests/unit/schema/postgresql.sql"
 
 if [[ $INSTALL_MEMCACHE == "yes" ]]; then phpenv config-add "$BASE/build/travis/phpenv/memcache.ini"; fi
 if [[ $INSTALL_MEMCACHED == "yes" ]]; then phpenv config-add "$BASE/build/travis/phpenv/memcached.ini"; fi
-if [[ $INSTALL_APC == "yes" ]]; then phpenv config-add "$BASE/build/travis/phpenv/apc-$TRAVIS_PHP_VERSION.ini"; fi
+if [[ $INSTALL_APC == "yes"  && $TRAVIS_PHP_VERSION != hhv* ]]; then phpenv config-add "$BASE/build/travis/phpenv/apc-$TRAVIS_PHP_VERSION.ini"; fi
 if [[ $INSTALL_APCU == "yes" && $TRAVIS_PHP_VERSION = 5.* ]]; then printf "\n" | pecl install apcu-4.0.10 && phpenv config-add "$BASE/build/travis/phpenv/apcu-$TRAVIS_PHP_VERSION.ini"; fi
 if [[ $INSTALL_APCU == "yes" && $TRAVIS_PHP_VERSION = 7.* ]]; then printf "\n" | pecl install apcu-beta && phpenv config-add "$BASE/build/travis/phpenv/apcu-$TRAVIS_PHP_VERSION.ini"; fi
 if [[ $INSTALL_APCU_BC_BETA == "yes" ]]; then printf "\n" | pecl install apcu_bc-beta; fi
-if [[ $INSTALL_REDIS == "yes" && $TRAVIS_PHP_VERSION != hhvm ]]; then phpenv config-add "$BASE/build/travis/phpenv/redis.ini"; fi
-if [[ $INSTALL_REDIS == "yes" && $TRAVIS_PHP_VERSION = hhvm ]]; then cat "$BASE/build/travis/phpenv/redis.ini" >> /etc/hhvm/php.ini; fi
+if [[ $INSTALL_REDIS == "yes" && $TRAVIS_PHP_VERSION != hhv* ]]; then phpenv config-add "$BASE/build/travis/phpenv/redis.ini"; fi
+if [[ $INSTALL_REDIS == "yes" && $TRAVIS_PHP_VERSION = hhv* ]]; then cat "$BASE/build/travis/phpenv/redis.ini" >> /etc/hhvm/php.ini; fi
