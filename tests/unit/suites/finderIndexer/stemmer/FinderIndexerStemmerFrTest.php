b6e8f5158726215e9b6677b84509f8fe54747677
photodude <walt@waltsorensen.com>
2016-09-14 21:24:19 -0600

    Revert "switch unset() for = null add parent::tearDown();"
    This reverts commit d7fa1458a2db98439cd673b9637e4f7f977ecd47.

--- tests/unit/suites/finderIndexer/stemmer/FinderIndexerStemmerFrTest.php
+++ tests/unit/suites/finderIndexer/stemmer/FinderIndexerStemmerFrTest.php
@@ -39,8 +39,7 @@ class FinderIndexerStemmerFrTest extends PHPUnit_Framework_TestCase
 	 */
 	protected function tearDown()
 	{
-		$this->object = null;
-		parent::tearDown();
+		unset($this->object);
 	}
 
 	/**
