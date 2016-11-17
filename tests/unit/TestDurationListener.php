9b11d79b7ce12edf488ff331ffb5886f59b8c3ad
Walt Sorensen <photodude@users.noreply.github.com>
2016-11-17 15:41:56 -0700

    silence listener

--- tests/unit/TestDurationListener.php
+++ tests/unit/TestDurationListener.php
@@ -3,6 +3,6 @@ class TestDurationListener extends PHPUnit_Framework_BaseTestListener
 {
     public function endTest(PHPUnit_Framework_Test $test, $time)
     {
-        echo "resource useage" . PHP_Timer::resourceUsage() . "\n";
+        // echo "resource useage" . PHP_Timer::resourceUsage() . "\n";
     }
 }
